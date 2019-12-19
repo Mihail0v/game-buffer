4d2dce3d89d21638c47542494c22080e4e0204a9
mihail0v <mihail0v@mail.com>
2019-12-19 18:36:15 +0700

    Search params for random game

--- src/UI/RandomGameController.php
+++ src/UI/RandomGameController.php
@@ -7,6 +7,7 @@ use App\Domain\Game\Exception\NotFoundException;
 use App\Domain\Game\Repository\GameRepository;
 use DateTimeImmutable;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -17,14 +18,20 @@ final class RandomGameController extends AbstractController
      * @Route("/random")
      * @param GameRepository $gameRepository
      * @param SerializerInterface $serializer
+     * @param Request $request
      * @return Response
      * @throws NotFoundException
      */
-    public function __invoke(GameRepository $gameRepository, SerializerInterface $serializer): Response
+    public function __invoke(GameRepository $gameRepository, SerializerInterface $serializer, Request $request): Response
     {
+        $startTime = $request->query->get('startTime');
+        $source = $request->query->get('source');
         return new Response(
             $serializer->serialize(
-                $gameRepository->getRandomGame(new DateTimeImmutable()),
+                $gameRepository->getRandomGame(
+                    $startTime === null ? null : DateTimeImmutable::createFromFormat('Y-m-d H:i:s', $startTime),
+                    $source
+                ),
                 'json'
             )
         );
